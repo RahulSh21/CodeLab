c804f9c6dae7c4c5ea2b49f5bf2000502bd98b0b
rahulshukla <rahul.shukla90@outlook.com>
2021-12-28 18:36:27 -0800

    Mode in Binary Search Tree.

--- ModeInBST/src/edu/northeastern/rahul/Main.java
+++ ModeInBST/src/edu/northeastern/rahul/Main.java
@@ -12,11 +12,11 @@ public class Main {
     private static  Node createBST(){
         Node root = new Node(8);
 
-        root.left = new Node(4);
-        root.right = new Node(3);
-        root.left.left = new Node(2);
+        root.left = new Node(9);
+        root.right = new Node(8);
+        root.left.left = new Node(9);
         root.left.right = new Node(3);
-        root.right.right = new Node(4);
+        root.right.right = new Node(9);
         root.left.right.left = new Node(2);
         root.left.right.right = new Node(1);
         root.right.right.left = new Node(4);
